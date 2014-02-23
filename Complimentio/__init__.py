d672e578183a888448f8afe27e354558598732e9
Misingnoglic <arya@aryaboudaie.com>
2014-02-23 07:05:55 -0500

    Added lists of random compliments plus error checkers

--- Complimentio/__init__.py
+++ Complimentio/__init__.py
@@ -7,15 +7,26 @@ account_sid = "AC66feb8a0982d83f73782a94069212ed7"
 auth_token  = "34ddec2af3dad454914b40280328c820"
 client = TwilioRestClient(account_sid, auth_token)
 
+
+
 def messageparse(body):
-    comps = ["Hey Sexy"]
     if " " in body:
-        return [body[:body.index(" ")],body[body.index(" ")+1:]]
-    else: return [body, comps[random.randint(0,len(comps))]]
+        stuff= [body[:body.index(" ")],body[body.index(" ")+1:]]
+        if stuff[1] == "*fun":
+            f = open("funnylist.txt","r")
+            comps = eval(f.read())
+            stuff= [body[:body.index(" ")], comps[random.randint(0,len(comps))-1]]
+
+    else:
+        f = open("reglist.txt","r")
+        comps = eval(f.read())
+        stuff= [body, comps[random.randint(0,len(comps))-1]]
+    return stuff
 
 
 def sendmessage(client, tonumber, compliment):
-    message = client.messages.create(body=compliment,
+    Msg = "Hello, you have been sent an anonymous compliment: "
+    message = client.messages.create(body=Msg+compliment,
         to=tonumber,
         from_="+16267747161",)
     #media_url="http://www.example.com/hearts.png")
@@ -29,12 +40,17 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
     print(request.args['Body'])
     stuff = messageparse(str(request.args['Body']))
-    sendmessage(client, stuff[0], stuff[1])
+    if stuff[0].isdigit() and (len(stuff[0])>8 and len(stuff[0])<12):
+        sendmessage(client, stuff[0], stuff[1])
+        msg = "Thank You, your message has been sent"
+    else: msg = "Please enter a 9-11 digit phone number (with no spaces) followed by a compliment. If you don't enter a compliment one will be chosen for you"
     resp = twilio.twiml.Response()
-    resp.message("Thank You, your message has been sent")#request.args['Body']
+    resp.message(msg)#request.args['Body']
     return str(resp)
 
 
  
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug=True)
+
+

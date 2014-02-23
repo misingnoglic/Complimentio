92fba0e1bfdede6291bbb050936550093ee1c0a2
seaurchi <seaurchi@brandeis.edu>
2014-02-23 03:11:27 -0500

    twiliotest now sends correct text body, dunno wtf is going on with run

--- run.py
+++ run.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
-    resp = twilio.twiml.Response()
-    
     #here is what you would fill with the body of the text sent to twilio
     resp.message("Hello, Mobile Monkey")
     return str(resp)
--- twoliotest.py
+++ twoliotest.py
@@ -15,15 +15,10 @@ def sendmessage(client, tonumber, compliment):
     print message.sid
 
 
-
-
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "AC66feb8a0982d83f73782a94069212ed7"
 auth_token  = "34ddec2af3dad454914b40280328c820"
 client = TwilioRestClient(account_sid, auth_token)
-sendmessage(client, "3233933245", "Your smile smells nice")
-#message = client.messages.create(body="Testing Text :)",
-#    to="+13233933245",
-#    from_="+16267747161",)
-    #media_url="http://www.example.com/hearts.png")
-#print message.sid
+textbody = messageparse(somemessage.body)
+sendmessage(client, "3233933245", textbody[1])
+#not aryas # but we're going to use his # to keep checking

eea08cecf69691d9b2c8253f8c25276e32d6a73a
seaurchi <seaurchi@brandeis.edu>
2014-02-23 03:48:46 -0500

    changed run  so it actually does stuff
    am not sure if it actually does stuff

--- run.py
+++ run.py
@@ -4,15 +4,29 @@ import twilio.twiml
 app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    #here is what you would fill with the body of the text sent to twilio
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
+
+def messageparse(body):
+	comps = ["Hey Sexy"]
+    if " " in body:
+        return [body[:body.index(" ")],body[body.index(" ")+1:]]
+    else: return [body, comps[random.rantint(0,len(comps))]]
+
+
+def sendmessage(client, tonumber, compliment):
+    message = client.messages.create(body=compliment,
+        to=tonumber,
+        from_="+16267747161",)
+    #media_url="http://www.example.com/hearts.png")
+    print message.sid
+
+
+account_sid = "AC66feb8a0982d83f73782a94069212ed7"
+auth_token  = "34ddec2af3dad454914b40280328c820"
+client = TwilioRestClient(account_sid, auth_token)
+
+textbody = messageparse(somemessage.body)
+sendmessage(client, "3233933245", textbody[1])
+#not aryas # but we're going to use his # to keep checking
  
 if __name__ == "__main__":
     app.run(debug=True)
-
-def messageparse(body):
-    if " " in body:
-        return [body[:body.index(" ")],body[body.index(" ")+1:]]
-    else: return [body]
--- twoliotest.py
+++ twoliotest.py
@@ -1,10 +1,13 @@
 # Download the Python helper library from twilio.com/docs/python/install
 from twilio.rest import TwilioRestClient
+import random 
+
  
 def messageparse(body):
+	comps = ["Hey Sexy"]
     if " " in body:
         return [body[:body.index(" ")],body[body.index(" ")+1:]]
-    else: return [body]
+    else: return [body, comps[random.rantint(0,len(comps))]]
 
 
 def sendmessage(client, tonumber, compliment):
@@ -15,10 +18,10 @@ def sendmessage(client, tonumber, compliment):
     print message.sid
 
 
-# Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "AC66feb8a0982d83f73782a94069212ed7"
 auth_token  = "34ddec2af3dad454914b40280328c820"
 client = TwilioRestClient(account_sid, auth_token)
+
 textbody = messageparse(somemessage.body)
 sendmessage(client, "3233933245", textbody[1])
 #not aryas # but we're going to use his # to keep checking
